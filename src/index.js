420f8960b8af58288dd54609dc5c0c33b96d647b
Matheus Barcelos <barcelosm1996@gmail.com>
2023-11-28 14:55:02 -0300

    adição do script do kubernetes deploy.yml

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import express from "express"
 const app = express();
 const port = parseInt(process.env.APP_PORT);
 
+app.get("/", (req, res) => {
+    res.send({
+        message: "Rule engine API working!"
+    });
+});
+
 app.listen(port, () => {
     console.log(`[app] server is listening at ${port} port`)
 });
